5ec0bf53410ee740f673e021aa92a50c6eb98eea
matrak63 <mathias.garcia@ipilyon.net>
2020-03-02 16:24:18 +0100

    Update class test

---
+++ src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.ipiecoles.java.java350.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeRepositoryTest {
+
+    @Test
+    void findLastMatricule() {
+    }
+}
